f655e44f78e6b60f3fb41f6876c798ed52c0d134
Lee Creighton <leecreighton@users.noreply.github.com>
2023-01-14 21:13:18 -0500

    Add R Markdown files, add JMP and SAS versions of .sas7bdat files

--- read_world_classes.R
+++ read_world_classes.R
@@ -5,34 +5,55 @@ library(haven)     # for sas7bdat
 library(arsenal)   # for comparing data frames
 
 
-#read CSV with dplyr::read_csv
+#read csv with dplyr::read_csv
 dplyr_read_csv <- read_csv("World Class.csv")
 
-#read csv with base::read.csv()
-base_read_csv <- read.csv("World Class.csv",
-                          check.names = FALSE)
-comparedf(dplyr_read_csv, base_read_csv) |> 
+#read csv with base::read.csv() with check names==T
+base_read_csv_T <- read.csv("World Class.csv")
+setdiff(dplyr_read_csv, base_read_csv_T)
+
+
+#read csv with base::read.csv() with check names==F
+base_read_csv_F <- read.csv("World Class.csv",
+                            check.names = FALSE)
+setdiff(dplyr_read_csv, base_read_csv_F)
+comparedf(dplyr_read_csv, base_read_csv_F) |> 
   summary()
 
-#read cssv with base::read.table()
+#read csv with base::read.table()
 base_read_table <- read.table("World Class.csv", 
                               sep=",", 
                               header=TRUE, 
                               check.names=FALSE)
+comparedf(dplyr_read_csv, base_read_table) |> 
+  summary()
 
 #read Tab-delimited values with dplyr::read_tsv
 dplyr_read_tsv <- read_tsv("World Class.txt")
+comparedf(dplyr_read_csv, dplyr_read_tsv) |> 
+  summary()
 
 #read JSON file with jsonlite::fromJSON()
-json_fromJSON <- fromJSON("World Class.json")
+jsonlite_fromJSON <- fromJSON("World Class.json")
+comparedf(dplyr_read_csv, jsonlite_fromJSON) |> 
+  summary()
 
 #read xlsx file with readxl::readxlsx
 readxl_read_xlsx <-  read_xlsx("World Class.xlsx")
 comparedf(dplyr_read_csv, readxl_read_xlsx) |> 
   summary()
 
-#read sasv7bdat file with haven::read_sas
-haven_read_sas <- read_sas("World_Class.sas7bdat")
-
 #read sas transport file with haven::read_xpt()
 haven_read_xpt <- read_xpt("WORLD_CL.stx")
+comparedf(dplyr_read_csv, haven_read_xpt) |> 
+  summary()
+
+#read SAS-generated sasv7bdat file with haven::read_sas
+haven_read_sas <- read_sas("World_Class_SAS.sas7bdat")
+comparedf(dplyr_read_csv, haven_read_sas) |> 
+  summary()
+
+#read JMP-generated sasv7bdat file with haven::read_sas
+haven_read_sas <- read_sas("World_Class_JMP.sas7bdat")
+comparedf(dplyr_read_csv, haven_read_sas) |> 
+  summary()
