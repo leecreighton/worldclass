ba586f85714e3e2c7f9470a199b3751cf0c237bd
leecreighton <lee.creighton@gmail.com>
2023-01-25 12:02:21 -0500

    Adjusted JSON file for a fairer comparison.

--- read_world_classes.R
+++ read_world_classes.R
@@ -1,5 +1,7 @@
 library(tidyverse) # for dplyr
 library(jsonlite)  # for json
+library(rjson)
+library(RJSONIO)
 library(readxl)    # for xlsx
 library(haven)     # for sas7bdat
 library(arsenal)   # for comparing data frames
@@ -9,35 +11,51 @@ dplyr_read_csv <- read_csv("World Class.csv")
 
 #read csv with base::read.csv() with check names==T
 base_read_csv_T <- read.csv("World Class.csv")
+colSums(base_read_csv_T != dplyr_read_csv)
 setdiff(dplyr_read_csv, base_read_csv_T)
 
 #read csv with base::read.csv() with check names==F
 base_read_csv_F <- read.csv("World Class.csv",
                             check.names = FALSE)
+colSums(base_read_csv_T != dplyr_read_csv)
 setdiff(dplyr_read_csv, base_read_csv_F)
-comparedf(dplyr_read_csv, base_read_csv_F) |> 
-  summary()
 
 #read csv with base::read.table()
 base_read_table <- read.table("World Class.csv", 
                               sep=",", 
                               header=TRUE, 
                               check.names=FALSE)
-comparedf(dplyr_read_csv, base_read_table) |> 
-  summary()
+colSums( base_read_table != dplyr_read_csv)
+setdiff(dplyr_read_csv, base_read_table)
 
 #read Tab-delimited values with dplyr::read_tsv
 dplyr_read_tsv <- read_tsv("World Class.txt")
-comparedf(dplyr_read_csv, dplyr_read_tsv) |> 
-  summary()
+colSums(base_read_table != dplyr_read_csv)
+setdiff(dplyr_read_csv, base_read_table)
 
 #read JSON file with jsonlite::fromJSON()
-jsonlite_fromJSON <- fromJSON("World Class.json")
-comparedf(dplyr_read_csv, jsonlite_fromJSON) |> 
-  summary()
+jsonlite_fromJSON <- jsonlite::fromJSON("World Class.json")
+colSums(jsonlite_fromJSON != dplyr_read_csv)
+setdiff(dplyr_read_csv, jsonlite_fromJSON)
+
+#read JSON file with rjson::fromJSON
+raw_JSON <- rjson::fromJSON(file = "World Class.json")
+rjson_fromJSON <- do.call(rbind.data.frame, raw_JSON)
+colSums(rjson_fromJSON != dplyr_read_csv)
+setdiff(dplyr_read_csv, rjson_fromJSON)
+
+# #read JSON file with RJSONIO::fromJSON
+# raw_JSON <- RJSONIO::fromJSON("World Class.json")
+# rjson_fromJSON <- do.call(rbind.data.frame, raw_JSON)
+# colSums(rjson_fromJSON != dplyr_read_csv)
+# setdiff(dplyr_read_csv, rjson_fromJSON)
+# comparedf(dplyr_read_csv, rjson_fromJSON) |> 
+#   summary()
 
 #read xlsx file with readxl::readxlsx
 readxl_read_xlsx <-  read_xlsx("World Class.xlsx")
+class(colSums(readxl_read_xlsx != dplyr_read_csv))
+setdiff(dplyr_read_csv, readxl_read_xlsx)
 comparedf(dplyr_read_csv, readxl_read_xlsx) |> 
   summary()
 
@@ -48,10 +66,10 @@ comparedf(dplyr_read_csv, haven_read_xpt) |>
 
 #read SAS-generated sasv7bdat file with haven::read_sas
 haven_read_sas <- read_sas("World_Class_SAS.sas7bdat")
+colSums(haven_read_sas != dplyr_read_csv)
+setdiff(dplyr_read_csv, haven_read_sas)
 comparedf(dplyr_read_csv, haven_read_sas) |> 
   summary()
 
 #read JMP-generated sasv7bdat file with haven::read_sas
 haven_read_sas <- read_sas("World_Class_JMP.sas7bdat")
-comparedf(dplyr_read_csv, haven_read_sas) |> 
-  summary()
